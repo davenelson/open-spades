4cebdeda0a64c63620aa8eea6b97c6002508bb2b
Dave Nelson <list@davenelson.com>
2013-06-05 16:15:51 -0400

    Added config for cards

---
+++ scripts/src/config.js
@@ -0,0 +1,5 @@
+var config = {
+    suits: ["hearts", "clubs", "diamonds", "spades"],
+    properName: ["two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "jack", "queen", "king", "ace"],
+    values: [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]
+}
