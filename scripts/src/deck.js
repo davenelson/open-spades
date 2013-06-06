a33fc13dc2361a18334e9746f5fd556a8c39c5bd
Dave Nelson <list@davenelson.com>
2013-06-05 21:11:03 -0400

    Updated to be a function

--- scripts/src/deck.js
+++ scripts/src/deck.js
@@ -1,30 +1,27 @@
-/*
- *
- *  A deck has the following properties
- *
+/**
+ * Creates a deck of 52 playing cards
+ * @returns {Array} - An array of card objects
  */
-
-var suit = null,
-    value = null,
-    properName = null,
-    cardCount = 0,
-    cards = [],
-    i = null,
-    x = null;
-
-for (i = 0; i <= 3; i++) { // suits
-    suit = config.suits[i];
-    for (x = 0; x <= 12; x++) { // values
-        value = config.values[x];
-        properName = config.properName[x];
-        cards[cardCount] = new Card(value, properName, suit);
-        // console.log((cardCount + 1) + " " + cards[cardCount].value + " / " + cards[cardCount].properName + " of " + cards[cardCount].suit);
-        cardCount++;
+function createDeck() {
+    "use strict";
+    var suit = null,
+        value = null,
+        properName = null,
+        cardCount = 0,
+        cards = [],
+        i = null,
+        x = null;
+    for (i = 0; i <= 3; i++) { // suits
+        suit = config.suits[i];
+        for (x = 0; x <= 12; x++) { // values
+            value = config.values[x];
+            properName = config.properName[x];
+            cards[cardCount] = new Card(value, properName, suit);
+            // console.log((cardCount + 1) + " " + cards[cardCount].value +
+            // " / " + cards[cardCount].properName + " of " +
+            // cards[cardCount].suit);
+            cardCount++;
+        }
     }
-}
-
-shuffle(cards);
-
-for (i = 0; i < cards.length; i++) {
-    console.log((i + 1) + " " + cards[i].value + " / " + cards[i].properName + " of " + cards[i].suit);
+    return cards;
 }
