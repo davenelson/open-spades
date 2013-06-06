d7e0e3c74a0e79bad0920eb87716624ed5ca48aa
Dave Nelson <list@davenelson.com>
2013-06-05 20:57:05 -0400

    Added a properly formatted JSDoc comment

--- scripts/src/shuffle.js
+++ scripts/src/shuffle.js
@@ -1,14 +1,12 @@
-/*
- *
- * Using the Fisher-Yates algorithm to shuffle the deck.
- *
- * Fisher-Yates
+/**
+ * Shuffles the properties of an array using the Fisher-Yates algorithm.
  * http://sedition.com/perl/javascript-fy.html
  * The Fisher–Yates shuffle (named after Ronald Fisher and Frank Yates), also
  * known as the Knuth shuffle (after Donald Knuth), is an algorithm for
  * generating a random permutation of a finite set—in plain terms, for randomly
  * shuffling the set.
- *
+ * @param myArray
+ * @returns {boolean}
  */
 function shuffle(myArray) {
     "use strict";
