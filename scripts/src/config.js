0182d5d4c86f891ce7b03b443e4766a40fd01665
Dave Nelson <list@davenelson.com>
2013-06-08 09:02:14 -0400

    Changed suits and properName to be title case

--- scripts/src/config.js
+++ scripts/src/config.js
@@ -1,5 +1,5 @@
 var config = {
-    suits: ["hearts", "clubs", "diamonds", "spades"],
-    properName: ["two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "jack", "queen", "king", "ace"],
+    suits: ["Hearts", "Clubs", "Diamonds", "Spades"],
+    properName: ["Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King", "Ace"],
     values: [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]
 };
